4e51d6807251101153b156ede07ad25f51f16bb2
druger <bayt32@yandex.ru>
2017-05-13 02:28:59 +0300

    Добавлен MVP для Reset password

--- app/src/main/java/com/druger/aboutwork/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/druger/aboutwork/activities/ResetPasswordActivity.java
@@ -1,42 +1,42 @@
 package com.druger.aboutwork.activities;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Patterns;
+import android.support.annotation.StringRes;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.druger.aboutwork.AboutWorkApp;
 import com.druger.aboutwork.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
+import com.druger.aboutwork.interfaces.view.ResetPasswordView;
+import com.druger.aboutwork.presenters.ResetPasswordPresenter;
 import com.squareup.leakcanary.RefWatcher;
 
-public class ResetPasswordActivity extends AppCompatActivity {
+public class ResetPasswordActivity extends MvpAppCompatActivity implements ResetPasswordView {
+
+    @InjectPresenter
+    ResetPasswordPresenter resetPasswordPresenter;
 
     private EditText etEmail;
     private ProgressBar progressBar;
-
-    private FirebaseAuth auth;
+    private Button btnResetPass;
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        Button btnResetPass = (Button) findViewById(R.id.btnResetPass);
-        Button btnBack = (Button) findViewById(R.id.btnBack);
-        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        resetPasswordPresenter.setAuth();
+        setUI();
+        setUX();
+    }
 
-        auth = FirebaseAuth.getInstance();
-
+    private void setUX() {
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,30 +47,17 @@ public class ResetPasswordActivity extends AppCompatActivity {
         btnResetPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-
                 String email = etEmail.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    Toast.makeText(getApplicationContext(), R.string.error_email, Toast.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE);
-                } else {
-                    auth.sendPasswordResetEmail(email)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(ResetPasswordActivity.this, R.string.sent_reset_pass, Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    } else {
-                                        Toast.makeText(ResetPasswordActivity.this, R.string.failed_reset_pass, Toast.LENGTH_SHORT).show();
-                                    }
-                                    progressBar.setVisibility(View.GONE);
-                                }
-                            });
-                }
+                resetPasswordPresenter.resetPassClick(email);
             }
         });
+    }
+
+    private void setUI() {
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        btnResetPass = (Button) findViewById(R.id.btnResetPass);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
     }
 
     @Override
@@ -78,5 +65,25 @@ public class ResetPasswordActivity extends AppCompatActivity {
         super.onDestroy();
         RefWatcher refWatcher = AboutWorkApp.getRefWatcher(this);
         refWatcher.watch(this);
+    }
+
+    @Override
+    public void showToast(@StringRes int resId) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void doResetPass() {
+        finish();
     }
 }
---
+++ app/src/main/java/com/druger/aboutwork/presenters/ResetPasswordPresenter.java
@@ -0,0 +1,54 @@
+package com.druger.aboutwork.presenters;
+
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+import com.druger.aboutwork.R;
+import com.druger.aboutwork.interfaces.view.ResetPasswordView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by druger on 13.05.2017.
+ */
+
+@InjectViewState
+public class ResetPasswordPresenter extends MvpPresenter<ResetPasswordView> {
+
+    private FirebaseAuth auth;
+
+    public void setAuth() {
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void resetPassClick(String email) {
+        resetPassword(email);
+    }
+
+    private void resetPassword(String email) {
+        getViewState().showProgress();
+
+        if (TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            getViewState().showToast(R.string.error_email);
+            getViewState().hideProgress();
+        } else {
+            auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                getViewState().showToast(R.string.sent_reset_pass);
+                                getViewState().doResetPass();
+                            } else {
+                                getViewState().showToast(R.string.failed_reset_pass);
+                            }
+                            getViewState().hideProgress();
+                        }
+                    });
+        }
+    }
+}
