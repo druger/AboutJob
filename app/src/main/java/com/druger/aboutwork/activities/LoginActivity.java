89556c17e844708e94ab318f0c296d1e407c5e46
druger <bayt32@yandex.ru>
2018-07-13 19:48:39 +0300

    AW-41. настройка firebase auth

--- app/src/main/java/com/druger/aboutwork/activities/LoginActivity.java
+++ app/src/main/java/com/druger/aboutwork/activities/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -19,33 +20,43 @@ import com.druger.aboutwork.App;
 import com.druger.aboutwork.R;
 import com.druger.aboutwork.interfaces.view.LoginView;
 import com.druger.aboutwork.presenters.LoginPresenter;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.squareup.leakcanary.RefWatcher;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends MvpAppCompatActivity implements LoginView {
     private static final int REQUEST_SIGNUP = 0;
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     @InjectPresenter
     LoginPresenter loginPresenter;
 
-    private EditText etEmail;
+    private EditText etEmailPhone;
     private EditText etPassword;
     private Button btnLogin;
     private ProgressBar progressBar;
     private TextView tvSignup;
     private EditText etForgot;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks verificationCallback;
+    private String verificationId;
+    private PhoneAuthProvider.ForceResendingToken resendtoken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginPresenter.setAuth(this);
-        setContentView(R.layout.activity_login_new);
+        setContentView(R.layout.activity_login);
         setupUI();
         setupUX();
-        setTextWatcher();
+//        setTextWatcher();
     }
 
     private void setTextWatcher() {
-        etEmail.addTextChangedListener(new TextWatcher() {
+        etEmailPhone.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -79,7 +90,7 @@ public class LoginActivity extends MvpAppCompatActivity implements LoginView {
     }
 
     private void checkCorrectFields() {
-        String email = etEmail.getText().toString().trim();
+        String email = etEmailPhone.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
         if (validate(email, password)) {
@@ -91,13 +102,12 @@ public class LoginActivity extends MvpAppCompatActivity implements LoginView {
 
     private void setupUX() {
         btnLogin.setOnClickListener(view -> {
-            String email = etEmail.getText().toString().trim();
-            String password = etPassword.getText().toString().trim();
+            String emailPhone = etEmailPhone.getText().toString().trim();
 
-            if (!validate(email, password)) {
-                onLoginFailed();
-            } else {
-                loginPresenter.loginClick(email, password);
+            if (Patterns.EMAIL_ADDRESS.matcher(emailPhone).matches()) {
+                emailAuth(emailPhone);
+            } else if (Patterns.PHONE.matcher(emailPhone).matches()) {
+                phoneAuth(emailPhone);
             }
         });
 
@@ -110,8 +120,67 @@ public class LoginActivity extends MvpAppCompatActivity implements LoginView {
                 startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class)));
     }
 
+    private void phoneAuth(String phone) {
+        initVerificationCallback();
+        FirebaseAuth.getInstance().setLanguageCode("ru");
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phone,
+                60,
+                TimeUnit.SECONDS,
+                this,
+                verificationCallback
+        );
+    }
+
+    private void initVerificationCallback() {
+        verificationCallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                Log.d(TAG, "onVerificationCompleted:" + phoneAuthCredential);
+                String smsCode = phoneAuthCredential.getSmsCode();
+                Toast.makeText(LoginActivity.this, smsCode, Toast.LENGTH_SHORT).show();
+
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Log.w(TAG, "onVerificationFailed", e);
+            }
+
+            @Override
+            public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                LoginActivity.this.verificationId = verificationId;
+                resendtoken = forceResendingToken;
+            }
+        };
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signInWithCredential:success");
+                        onLoginSuccess();
+                    } else {
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    }
+                });
+    }
+
+    private void emailAuth(String email) {
+        String password = etPassword.getText().toString().trim();
+
+        if (!validate(email, password)) {
+            onLoginFailed();
+        } else {
+            loginPresenter.loginClick(email, password);
+        }
+    }
+
     private void setupUI() {
-        etEmail = findViewById(R.id.etEmail);
+        etEmailPhone = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         tvSignup = findViewById(R.id.tvSignup);
@@ -141,19 +210,19 @@ public class LoginActivity extends MvpAppCompatActivity implements LoginView {
 
     @Override
     public void onLoginSuccess() {
-        btnLogin.setEnabled(true);
+//        btnLogin.setEnabled(true);
         showMainActivity();
     }
 
     @Override
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), R.string.login_failed, Toast.LENGTH_SHORT).show();
-        btnLogin.setEnabled(true);
+//        btnLogin.setEnabled(true);
     }
 
     @Override
     public void showProgress() {
-        btnLogin.setEnabled(false);
+//        btnLogin.setEnabled(false);
         progressBar.setVisibility(View.VISIBLE);
     }
 
@@ -166,7 +235,8 @@ public class LoginActivity extends MvpAppCompatActivity implements LoginView {
         boolean valid = true;
 
         if (TextUtils.isEmpty(email) ||
-                !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                || !Patterns.PHONE.matcher(email).matches()) {
             valid = false;
         }
 
