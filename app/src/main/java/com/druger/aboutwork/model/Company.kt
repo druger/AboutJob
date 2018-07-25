3e18ddbb69bea564ca71eaaea13079b9c850ea5c
druger <bayt32@yandex.ru>
2018-07-25 19:59:30 +0300

    AW-42. Исправлен баг с вылетом при входе в раздел "Мои отзывы"

--- app/src/main/java/com/druger/aboutwork/model/Company.kt
+++ app/src/main/java/com/druger/aboutwork/model/Company.kt
@@ -8,7 +8,7 @@ import com.google.gson.annotations.SerializedName
  * Created by druger on 24.07.2016.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-class Company(var id: String, var name: String) {
+class Company(var id: String = "", var name: String = "") {
     @JsonIgnore
     @SerializedName("logo_urls")
     var logo: Logo? = null
