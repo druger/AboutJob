15b3353665ec1a25f02b5358e1ba3e82ce803bf7
Evgeny Gromov <bayt32@yandex.ru>
2019-09-30 02:12:12 +0300

    AJ-97. fix duplicate reviews after deleting

--- app/src/main/java/com/druger/aboutwork/model/Review.kt
+++ app/src/main/java/com/druger/aboutwork/model/Review.kt
@@ -1,8 +1,5 @@
 package com.druger.aboutwork.model
 
-import android.os.Parcel
-import android.os.Parcelable
-
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
@@ -10,13 +7,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
  * Created by druger on 10.08.2016.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-class Review : Parcelable {
+data class Review(var companyId: String? = null,
+                  var userId: String? = null,
+                  var date: Long = 0) {
 
-    var companyId: String? = null
-    var userId: String? = null
     @JsonIgnore
     var name: String? = null
-    var date: Long = 0
     var pluses: String? = null
     var minuses: String? = null
     var markCompany: MarkCompany? = null
@@ -33,70 +29,13 @@ class Review : Parcelable {
     @JsonIgnore
     var firebaseKey: String? = null
 
-    constructor() {}
-
-    constructor(companyId: String, userId: String, date: Long) {
-        this.companyId = companyId
-        this.userId = userId
-        this.date = date
-    }
-
-    protected constructor(`in`: Parcel) {
-        companyId = `in`.readString()
-        userId = `in`.readString()
-        name = `in`.readString()
-        date = `in`.readLong()
-        pluses = `in`.readString()
-        minuses = `in`.readString()
-        status = `in`.readInt()
-        city = `in`.readString()
-        position = `in`.readString()
-        employmentDate = `in`.readLong()
-        dismissalDate = `in`.readLong()
-        interviewDate = `in`.readLong()
-        like = `in`.readInt()
-        dislike = `in`.readInt()
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(companyId)
-        dest.writeString(userId)
-        dest.writeString(name)
-        dest.writeLong(date)
-        dest.writeString(pluses)
-        dest.writeString(minuses)
-        dest.writeInt(status)
-        dest.writeString(city)
-        dest.writeString(position)
-        dest.writeLong(employmentDate)
-        dest.writeLong(dismissalDate)
-        dest.writeLong(interviewDate)
-        dest.writeInt(like)
-        dest.writeInt(dislike)
-    }
 
     companion object {
-
         /**
          * Статусы работника
          */
         const val WORKING = 0
         const val WORKED = 1
         const val INTERVIEW = 2
-
-        @JvmField
-        val CREATOR: Parcelable.Creator<Review> = object : Parcelable.Creator<Review> {
-            override fun createFromParcel(`in`: Parcel): Review {
-                return Review(`in`)
-            }
-
-            override fun newArray(size: Int): Array<Review?> {
-                return arrayOfNulls(size)
-            }
-        }
     }
 }
