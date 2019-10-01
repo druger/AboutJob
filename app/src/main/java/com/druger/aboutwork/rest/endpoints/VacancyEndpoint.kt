f56f96c814508d4a6a74d30a8d274f93f323e49b
Evgeny Gromov <bayt32@yandex.ru>
2019-10-02 01:46:23 +0300

    AJ-34. Observable replaced by Single

--- app/src/main/java/com/druger/aboutwork/rest/endpoints/VacancyEndpoint.kt
+++ app/src/main/java/com/druger/aboutwork/rest/endpoints/VacancyEndpoint.kt
@@ -1,11 +1,11 @@
 package com.druger.aboutwork.rest.endpoints
 
 import com.druger.aboutwork.rest.models.VacancyResponse
-import io.reactivex.Observable
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface VacancyEndpoint {
     @GET("/suggests/vacancy_search_keyword")
-    fun getVacancies(@Query("text") position: String): Observable<VacancyResponse>
+    fun getVacancies(@Query("text") position: String): Single<VacancyResponse>
 }
