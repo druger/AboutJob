a5e7b584f2d1d30d5d4e936602153caf47cd0482
Gromov Evgeny <e.gromov@key-g.com>
2020-02-04 02:10:26 +0300

    AJ-4: setup suggestions

--- app/src/main/java/com/druger/aboutwork/presenters/FilterPresenter.kt
+++ app/src/main/java/com/druger/aboutwork/presenters/FilterPresenter.kt
@@ -2,9 +2,17 @@ package com.druger.aboutwork.presenters
 
 import com.druger.aboutwork.App
 import com.druger.aboutwork.interfaces.view.FilterView
+import com.druger.aboutwork.rest.RestApi
+import com.druger.aboutwork.rest.models.CityResponse
+import com.druger.aboutwork.rest.models.VacancyResponse
+import com.druger.aboutwork.utils.rx.RxUtils
 import javax.inject.Inject
 
-class FilterPresenter @Inject constructor() : BasePresenter<FilterView>() {
+class FilterPresenter @Inject constructor(restApi: RestApi) : BasePresenter<FilterView>() {
+
+    init {
+        this.restApi = restApi
+    }
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
@@ -41,5 +49,27 @@ class FilterPresenter @Inject constructor() : BasePresenter<FilterView>() {
 
     fun filterAtPopularity() {
 
+    }
+
+    fun getCities(city: String) {
+        val request = restApi.cities.getCities(city)
+            .compose(RxUtils.observableTransformer())
+            .subscribe({ this.successGetCities(it) }, { this.handleError(it) })
+        unSubscribeOnDestroy(request)
+    }
+
+    private fun successGetCities(cityResponse: CityResponse) {
+        cityResponse.items?.let { viewState.showCities(cityResponse.items) }
+    }
+
+    fun getPositions(position: String) {
+        val request = restApi.vacancies.getVacancies(position)
+            .compose(RxUtils.observableTransformer())
+            .subscribe({ this.successGetVacancies(it) }, { this.handleError(it) })
+        unSubscribeOnDestroy(request)
+    }
+
+    private fun successGetVacancies(vacancyResponse: VacancyResponse) {
+        vacancyResponse.items?.let { viewState.showPositions(vacancyResponse.items) }
     }
 }
