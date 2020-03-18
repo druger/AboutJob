152a52f20769b839a09064ba0c09f741584c0653
Gromov Evgeny <e.gromov@key-g.com>
2020-03-19 02:04:49 +0300

    AJ-159: refactoring

--- app/src/main/java/com/druger/aboutwork/presenters/MyReviewsPresenter.kt
+++ app/src/main/java/com/druger/aboutwork/presenters/MyReviewsPresenter.kt
@@ -1,26 +1,37 @@
 package com.druger.aboutwork.presenters
 
+import com.druger.aboutwork.App
 import com.druger.aboutwork.db.FirebaseHelper
 import com.druger.aboutwork.interfaces.view.MyReviewsView
 import com.druger.aboutwork.model.Company
 import com.druger.aboutwork.model.Review
+import com.druger.aboutwork.utils.Analytics
 import com.google.firebase.database.*
 import moxy.InjectViewState
 import moxy.MvpPresenter
 import timber.log.Timber
 import java.util.*
+import javax.inject.Inject
 
 /**
  * Created by druger on 09.05.2017.
  */
 
 @InjectViewState
-class MyReviewsPresenter : MvpPresenter<MyReviewsView>(), ValueEventListener {
+class MyReviewsPresenter @Inject constructor() : MvpPresenter<MyReviewsView>(), ValueEventListener {
+
+    @Inject
+    lateinit var analytics: Analytics
 
     private lateinit var dbReference: DatabaseReference
     private var valueEventListener: ValueEventListener? = null
 
     private val reviews = ArrayList<Review>()
+
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        App.appComponent.inject(this)
+    }
 
     fun fetchReviews(userId: String) {
         viewState.showProgress(true)
@@ -79,7 +90,12 @@ class MyReviewsPresenter : MvpPresenter<MyReviewsView>(), ValueEventListener {
         }
     }
 
-    fun removeListeners() {
+    override fun onDestroy() {
+        super.onDestroy()
+        removeListeners()
+    }
+
+    private fun removeListeners() {
         valueEventListener?.let {
             dbReference.removeEventListener(this)
             dbReference.removeEventListener(it)
@@ -104,5 +120,9 @@ class MyReviewsPresenter : MvpPresenter<MyReviewsView>(), ValueEventListener {
 
     fun addToFirebase(review: Review) {
         FirebaseHelper.addReview(review)
+    }
+
+    fun logEvent(event: String) {
+        analytics.logEvent(event)
     }
 }
